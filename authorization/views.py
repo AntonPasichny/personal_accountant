b2b62125e08d4e28bf7bbb0176dcb65c3ab7aea0
antonpasichny <pasichny.law@gmail.com>
2021-12-24 13:50:20 +0200

    Initial commit

---
+++ authorization/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+
+class Authorization(View):
+
+    def get(self, request):
+        return render(request, 'authorization/authorization.html', {})
